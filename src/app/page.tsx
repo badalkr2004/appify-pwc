de7ff8fad50615f184bc0d046d8d406d1e81f507
badalkr2004 <badalkr279@gmail.com>
2025-02-26 12:44:07 +0530

    configure clerk admin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
 import { NavigationMenuMain } from "@/components/landing/navbar";
 import SustainableProducts from "@/components/landing/product";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import cover_image from "../../public/front-page/main-cover-image.jpg"
 
+import Image from "next/image";
+import cover_image from "../../public/front-page/main-cover-image.jpg";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-start ">
       <div className="p-7">
-      <NavigationMenuMain/>
+        <NavigationMenuMain />
       </div>
-      <Image src={cover_image} alt="hero" width={600} height={400}/>
-      <SustainableProducts/>
-      
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <Image src={cover_image} alt="hero" width={600} height={400} />
+      <SustainableProducts />
     </div>
   );
 }
