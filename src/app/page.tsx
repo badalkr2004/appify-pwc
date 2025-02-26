60a0da3796efaa8fd7917fda5f50c4c38ba9afe6
fendrick <its.fendrick@gmail.com>
2025-02-26 11:55:10 +0530

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import { NavigationMenuMain } from "@/components/landing/navbar";
+import SustainableProducts from "@/components/landing/product";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import cover_image from "../../public/front-page/main-cover-image.jpg"
+
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
-      <Button className="bg-green-700">Let's Build it</Button>
+    <div className="flex flex-col items-center justify-start ">
+      <div className="p-7">
+      <NavigationMenuMain/>
+      </div>
+      <Image src={cover_image} alt="hero" width={600} height={400}/>
+      <SustainableProducts/>
+      
     </div>
   );
 }
